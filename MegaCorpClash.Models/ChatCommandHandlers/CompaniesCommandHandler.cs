6ec201fde20bb4f006ae2b10d9ba4d2be0a8e445
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-08-18 17:58:37 -0500

    Added !companies chat command

---
+++ MegaCorpClash.Models/ChatCommandHandlers/CompaniesCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using TwitchLib.Client.Models;
+
+namespace MegaCorpClash.Models.ChatCommandHandlers;
+
+public class CompaniesCommandHandler : BaseCommandHandler, IHandleChatCommand
+{
+    public string CommandText => "companies";
+
+    public CompaniesCommandHandler(Dictionary<string, Player> players)
+        : base(players)
+    {
+    }
+
+    public void Execute(ChatCommand chatCommand)
+    {
+        InvokeMessageToDisplay(chatCommand, 
+            $"Companies: {string.Join(", ", _players.Values.OrderBy(c => c.CompanyName).Select(c => $"{c.CompanyName} ({c.DisplayName})"))}");
+    }
+}
