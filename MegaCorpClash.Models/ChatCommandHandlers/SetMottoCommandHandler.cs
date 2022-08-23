65b49e87cbc445735377e8c8eee462aa140e3c5b
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-08-23 16:25:36 -0500

    Added setmotto command and Motto property on Player class

---
+++ MegaCorpClash.Models/ChatCommandHandlers/SetMottoCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MegaCorpClash.Models.ExtensionMethods;
+using TwitchLib.Client.Models;
+
+namespace MegaCorpClash.Models.ChatCommandHandlers;
+
+public class SetMottoCommandHandler : BaseCommandHandler, IHandleChatCommand
+{
+    public string CommandName => "setmotto";
+
+    public SetMottoCommandHandler(Dictionary<string, Player> players)
+        : base(players)
+    {
+    }
+
+    public void Execute(ChatCommand chatCommand)
+    {
+        string chatterDisplayName = chatCommand.ChatterDisplayName();
+        Player? player = GetPlayerObjectForChatter(chatCommand);
+
+        if (string.IsNullOrWhiteSpace(chatCommand.ArgumentsAsString))
+        {
+            PublishMessage(chatterDisplayName, "You must enter a value for the motto");
+            return;
+        }
+
+        if (player == null)
+        {
+            PublishMessage(chatterDisplayName, "You do not have a company");
+            return;
+        }
+
+        player.Motto = chatCommand.ArgumentsAsString;
+        NotifyPlayerDataUpdated();
+        PublishMessage(chatterDisplayName,
+                $"Your new company motto is '{player.Motto}'");
+    }
+}
