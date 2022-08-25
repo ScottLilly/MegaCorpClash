f7423573a23964aabbfad1c9a7861c0591b3aa01
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-08-25 17:14:35 -0500

    Moved points and turn values to appsettings

--- MegaCorpClash.Models/GameSettings.cs
+++ MegaCorpClash.Models/GameSettings.cs
@@ -6,12 +6,24 @@ public class GameSettings
     public string BotAccountName { get; set; }
     public string TwitchToken { get; set; }
     public string PointsName { get; set; }
-    public int MinutesPerTurn { get; set; }
+    public TurnInfo TurnDetails { get; set; }
     public TimedMessageSettings TimedMessages { get; set; }
 
     public class TimedMessageSettings
     {
         public int IntervalInMinutes { get; set; }
         public List<string> Messages { get; set; }
+    }
+
+    public class TurnInfo
+    {
+        public int MinutesPerTurn { get; set; }
+        public PointsInfo PointsPerTurn { get; set; }
+    }
+
+    public class PointsInfo
+    {
+        public int Lurker { get; set; }
+        public int Chatter { get; set; }
     }
 }
