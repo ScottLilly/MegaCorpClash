3fdce346779ab7f95530f1e55d0fae7b630bd858
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-08-31 17:08:05 -0500

    Added !jobs command

---
+++ MegaCorpClash.Models/ChatCommandHandlers/JobsCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MegaCorpClash.Models.ExtensionMethods;
+using TwitchLib.Client.Models;
+
+namespace MegaCorpClash.Models.ChatCommandHandlers;
+
+public class JobsCommandHandler : BaseCommandHandler
+{
+    readonly string _jobList;
+
+    public JobsCommandHandler(GameSettings gameSettings,
+        Dictionary<string, Player> players)
+        : base("jobs", gameSettings, players)
+    {
+        _jobList =
+            string.Join(", ",
+            GameSettings
+            .EmployeeHiringDetails
+            .OrderBy(e => e.EmployeeType)
+            .Select(e => $"{e.EmployeeType} ({e.CostToHire})"));
+    }
+
+    public override void Execute(ChatCommand chatCommand)
+    {
+        PublishMessage(chatCommand.ChatterDisplayName(),
+            $"Job description and cost to hire: {_jobList}");
+    }
+}
