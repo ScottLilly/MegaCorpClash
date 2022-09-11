4b2205a911c0af33aa307908e2b1022c20e472d6
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-09-11 08:23:49 -0500

    Moved argument parsing to separate class

---
+++ MegaCorpClash.Core/ArgumentParser.cs
@@ -0,0 +1,60 @@
+﻿namespace MegaCorpClash.Core;
+
+public class ArgumentParser
+{
+    private readonly char[] _separators;
+
+    public ArgumentParser() : this(new[] { ' ' })
+    {
+    }
+
+    public ArgumentParser(char[] separators)
+    {
+        _separators = separators;
+    }
+
+    public ParsedArguments Parse(string arguments)
+    {
+        // Initial split of arguments
+        var splitArguments =
+            arguments.Split(_separators,
+                    StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+        // Find arguments that are integers or decimals
+        var integerArgumentsAsStrings =
+            splitArguments
+                .Where(a => int.TryParse(a, out _))
+                .ToList();
+
+        var decimalArgumentsAsStrings =
+            splitArguments
+                .Except(integerArgumentsAsStrings)
+                .Where(a => decimal.TryParse(a, out _))
+                .ToList();
+
+        // Variables for arguments by datatype
+        var integerArguments =
+            integerArgumentsAsStrings
+                .Select(int.Parse)
+                .ToList();
+
+        var decimalArguments =
+            decimalArgumentsAsStrings
+                .Select(decimal.Parse)
+                .ToList();
+
+        var stringArguments =
+            splitArguments
+                .Except(integerArgumentsAsStrings)
+                .Except(decimalArgumentsAsStrings)
+                .ToList();
+
+        return new
+            ParsedArguments(
+                splitArguments,
+                integerArguments,
+                decimalArguments,
+                stringArguments);
+    }
+}
