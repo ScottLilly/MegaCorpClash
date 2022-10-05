fcaad79125e3cd063bc2b2a025abdb6250cf559a
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-10-04 19:23:15 -0500

    Added !steal command

--- MegaCorpClash.Models/ChatCommandHandlers/StealCommandHandler.cs
+++ MegaCorpClash.Models/ChatCommandHandlers/StealCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using CSharpExtender.ExtensionMethods;
+using CSharpExtender.Services;
 using MegaCorpClash.Models.CustomEventArgs;
+using Newtonsoft.Json.Linq;
 
 namespace MegaCorpClash.Models.ChatCommandHandlers
 {
@@ -22,7 +24,7 @@ namespace MegaCorpClash.Models.ChatCommandHandlers
                 return;
             }
 
-            // Check player has a Spy
+            // Check if player has a Spy
             if (chatter.Company.Employees.None(e => e.Type == EmployeeType.Spy))
             {
                 PublishMessage(chatter.ChatterName,
@@ -30,6 +32,71 @@ namespace MegaCorpClash.Models.ChatCommandHandlers
                 return;
             }
 
+            // "Consume" spy
+            var spyEmployeeQuantity = 
+                chatter.Company.Employees
+                    .First(e => e.Type == EmployeeType.Spy);
+
+            if (spyEmployeeQuantity.Quantity == 1)
+            {
+                chatter.Company.Employees.Remove(spyEmployeeQuantity);
+            }
+            else
+            {
+                spyEmployeeQuantity.Quantity--;
+            }
+
+            var broadcasterCompany = GetBroadcasterCompany;
+
+            // Determine success of attack
+            int securityEmployeeCount =
+                broadcasterCompany.Employees
+                    .Where(e => e.Type == EmployeeType.Security)
+                    .Sum(e => e.Quantity)
+                + 1;
+
+            int broadcasterDefenseBonus = 
+                Math.Max(25, Convert.ToInt32(Math.Log10(securityEmployeeCount) * 10));
+
+            int rand = RngCreator.GetNumberBetween(1, 100);
+
+            if (rand > 50 + broadcasterDefenseBonus)
+            {
+                // Success
+                int stolen = (int)broadcasterCompany.Points / 100;
+                //(int)Math.Min(2500, broadcasterCompany.Points);
+
+                chatter.Company.Points += stolen;
+                broadcasterCompany.Points -= stolen;
+
+                PublishMessage(chatter.ChatterName,
+                    $"You stole {stolen} {GameSettings.PointsName} from {broadcasterCompany.CompanyName}");
+            }
+            else
+            {
+                // failure
+                // "Consume" broadcaster security person
+                var securityEmployeeQuantity =
+                    broadcasterCompany.Employees
+                        .FirstOrDefault(e => e.Type == EmployeeType.Security);
+
+                if (securityEmployeeQuantity == null)
+                {
+                }
+                else if(securityEmployeeQuantity.Quantity == 1)
+                {
+                    broadcasterCompany.Employees.Remove(securityEmployeeQuantity);
+                }
+                else
+                {
+                    securityEmployeeQuantity.Quantity--;
+                }
+
+                PublishMessage(chatter.ChatterName,
+                    $"You failed to steal from {broadcasterCompany.CompanyName}");
+            }
+
+            NotifyPlayerDataUpdated();
         }
     }
 }
