45d1d5814a408e316aeadd43de588d0de6739153
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-10-25 18:15:30 -0500

    Added broadcaster-only !tax command

---
+++ MegaCorpClash.Models/BroadcasterCommandHandlers/BroadcasterOnlyCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MegaCorpClash.Models.ChatCommandHandlers;
+
+namespace MegaCorpClash.Models.BroadcasterCommandHandlers;
+
+public abstract class BroadcasterOnlyCommandHandler : BaseCommandHandler
+{
+    protected BroadcasterOnlyCommandHandler(
+        string commandName, GameSettings gameSettings, Dictionary<string, Company> companies) 
+        : base(commandName, gameSettings, companies)
+    {
+    }
+}
---
+++ MegaCorpClash.Models/BroadcasterCommandHandlers/TaxCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MegaCorpClash.Models.CustomEventArgs;
+
+namespace MegaCorpClash.Models.BroadcasterCommandHandlers;
+
+public class TaxCommandHandler : BroadcasterOnlyCommandHandler
+{
+    public TaxCommandHandler(GameSettings gameSettings,
+        Dictionary<string, Company> companies)
+        : base("tax", gameSettings, companies)
+    {
+    }
+
+    public override void Execute(GameCommandArgs gameCommandArgs)
+    {
+        if (!gameCommandArgs.IsBroadcaster)
+        {
+            return;
+        }
+
+        var parsedArguments =
+            _argumentParser.Parse(gameCommandArgs.Argument);
+
+        if (parsedArguments.IntegerArguments.Count != 1 ||
+            parsedArguments.IntegerArguments.First() < 1 ||
+            parsedArguments.IntegerArguments.First() > 99)
+        {
+            PublishMessage("Tax command requires a single integer tax rate between 1 and 99");
+            return;
+        }
+
+        foreach (var company in Companies
+            .Where(c => !c.Value.IsBroadcaster)
+            .Select(c => c.Value))
+        {
+            company.Points =
+                Convert.ToInt32(
+                    company.Points *
+                    ((100M - parsedArguments.IntegerArguments.First()) / 100M));
+        }
+
+        PublishMessage($"A tax of {parsedArguments.IntegerArguments.First()} has been applied");
+
+        NotifyPlayerDataUpdated();
+    }
+}
