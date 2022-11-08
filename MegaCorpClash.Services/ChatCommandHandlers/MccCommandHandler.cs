e457e79e4d8aea42b001348c94bb2198a8da4e13
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-11-08 17:34:09 -0600

    Added !mcc command

---
+++ MegaCorpClash.Services/ChatCommandHandlers/MccCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using MegaCorpClash.Models;
+using MegaCorpClash.Services.CustomEventArgs;
+
+namespace MegaCorpClash.Services.ChatCommandHandlers;
+
+public class MccCommandHandler : BaseCommandHandler
+{
+    public MccCommandHandler(GameSettings gameSettings,
+        Dictionary<string, Company> companies)
+        : base("mcc", gameSettings, companies)
+    {
+    }
+
+    public override void Execute(GameCommandArgs gameCommandArgs)
+    {
+        PublishMessage(@"Instructions for MegaCorpClash are at https://megacorpclash.com/game-instructions/");
+    }
+}
