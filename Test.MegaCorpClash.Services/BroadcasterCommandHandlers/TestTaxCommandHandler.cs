bef491e5bea06d76e8c16a7a9e1cd81e14fb73dd
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-12-19 15:48:48 -0600

    Completed current set of unit tests

--- Test.MegaCorpClash.Services/BroadcasterCommandHandlers/TestTaxCommandHandler.cs
+++ Test.MegaCorpClash.Services/BroadcasterCommandHandlers/TestTaxCommandHandler.cs
@@ -1,142 +1,87 @@
-﻿using MegaCorpClash.Models;
-using MegaCorpClash.Services.BroadcasterCommandHandlers;
-using MegaCorpClash.Services.CustomEventArgs;
+﻿using MegaCorpClash.Services.BroadcasterCommandHandlers;
+using Shouldly;
 using Test.MegaCorpClash.Services.ChatCommandHandlers;
 
 namespace Test.MegaCorpClash.Services.BroadcasterCommandHandlers;
 
 public class TestTaxCommandHandler : BaseCommandHandlerTest
 {
-    private readonly GameSettings _gameSettings =
-        GetDefaultGameSettings();
-
     [Fact]
     public void Test_NotBroadcaster()
     {
-        //Dictionary<string, Company> companies = new();
-        //companies.Add("123321",
-        //    new Company
-        //    {
-        //        DisplayName = "NOT_BROADCASTER",
-        //        CompanyName = "NOT_BROADCASTER",
-        //        Points = 100
-        //    });
+        var repo = GetTestInMemoryRepository();
+        var company = repo.GetCompany("101");
+        var args = GetGameCommandArgs(company, "tax", "1000");
 
-        //var commandHandler =
-        //    new TaxCommandHandler(_gameSettings, companies);
+        var commandHandler =
+            new TaxCommandHandler(GetDefaultGameSettings(), repo, args);
 
-        //var gameCommandArgs =
-        //    new GameCommandArgs("123321", "NOT_BROADCASTER", "tax", "5", false, false, false, false);
+        commandHandler.Execute();
 
-        //commandHandler.Execute(gameCommandArgs);
-
-        //Assert.Empty(commandHandler.ChatMessages);
-        //Assert.False(commandHandler.CompanyDataUpdated);
+        commandHandler.ChatMessages.ShouldBeEmpty();
     }
 
     [Fact]
     public void Test_TaxBadParameter_NegativeNumber()
     {
-        //Dictionary<string, Company> companies = new();
-        //companies.Add("123321",
-        //    new Company
-        //    {
-        //        DisplayName = "BROADCASTER",
-        //        CompanyName = "BROADCASTER",
-        //        Points = 100
-        //    });
+        var repo = GetTestInMemoryRepository();
+        var company = repo.GetBroadcasterCompany();
+        var args = GetGameCommandArgs(company, "tax", "-1");
 
-        //var commandHandler =
-        //    new TaxCommandHandler(_gameSettings, companies);
+        var commandHandler =
+            new TaxCommandHandler(GetDefaultGameSettings(), repo, args);
 
-        //var gameCommandArgs =
-        //    new GameCommandArgs("123321", "BROADCASTER", "tax", "-1", true, false, false, false);
+        commandHandler.Execute();
 
-        //commandHandler.Execute(gameCommandArgs);
-
-        //Assert.Equal("Tax command requires a single integer tax rate between 1 and 99", commandHandler.ChatMessages.First());
-        //Assert.False(commandHandler.CompanyDataUpdated);
+        commandHandler.ChatMessages.First()
+            .ShouldBe("Tax command requires a single integer tax rate between 1 and 99");
     }
 
     [Fact]
     public void Test_TaxBadParameter_Zero()
     {
-        //Dictionary<string, Company> companies = new();
-        //companies.Add("123321",
-        //    new Company
-        //    {
-        //        DisplayName = "BROADCASTER",
-        //        CompanyName = "BROADCASTER",
-        //        Points = 100
-        //    });
+        var repo = GetTestInMemoryRepository();
+        var company = repo.GetBroadcasterCompany();
+        var args = GetGameCommandArgs(company, "tax", "0");
 
-        //var commandHandler =
-        //    new TaxCommandHandler(_gameSettings, companies);
+        var commandHandler =
+            new TaxCommandHandler(GetDefaultGameSettings(), repo, args);
 
-        //var gameCommandArgs =
-        //    new GameCommandArgs("123321", "BROADCASTER", "tax", "0", true, false, false, false);
+        commandHandler.Execute();
 
-        //commandHandler.Execute(gameCommandArgs);
-
-        //Assert.Equal("Tax command requires a single integer tax rate between 1 and 99", commandHandler.ChatMessages.First());
-        //Assert.False(commandHandler.CompanyDataUpdated);
+        commandHandler.ChatMessages.First()
+            .ShouldBe("Tax command requires a single integer tax rate between 1 and 99");
     }
 
     [Fact]
     public void Test_TaxBadParameter_GreaterThan99()
     {
-        //Dictionary<string, Company> companies = new();
-        //companies.Add("123321",
-        //    new Company
-        //    {
-        //        DisplayName = "BROADCASTER",
-        //        CompanyName = "BROADCASTER",
-        //        Points = 100
-        //    });
+        var repo = GetTestInMemoryRepository();
+        var company = repo.GetBroadcasterCompany();
+        var args = GetGameCommandArgs(company, "tax", "100");
 
-        //var commandHandler =
-        //    new TaxCommandHandler(_gameSettings, companies);
+        var commandHandler =
+            new TaxCommandHandler(GetDefaultGameSettings(), repo, args);
 
-        //var gameCommandArgs =
-        //    new GameCommandArgs("123321", "BROADCASTER", "tax", "100", true, false, false, false);
+        commandHandler.Execute();
 
-        //commandHandler.Execute(gameCommandArgs);
-
-        //Assert.Equal("Tax command requires a single integer tax rate between 1 and 99", commandHandler.ChatMessages.First());
-        //Assert.False(commandHandler.CompanyDataUpdated);
+        commandHandler.ChatMessages.First()
+            .ShouldBe("Tax command requires a single integer tax rate between 1 and 99");
     }
 
     [Fact]
     public void Test_TaxValid()
     {
-        //Dictionary<string, Company> companies = new();
-        //companies.Add("123321",
-        //    new Company
-        //    {
-        //        IsBroadcaster = true,
-        //        DisplayName = "BROADCASTER",
-        //        CompanyName = "BROADCASTER",
-        //        Points = 100
-        //    });
-        //companies.Add("123322",
-        //    new Company
-        //    {
-        //        DisplayName = "CHATTER",
-        //        CompanyName = "CHATTER",
-        //        Points = 100
-        //    });
+        var repo = GetTestInMemoryRepository();
+        var company = repo.GetBroadcasterCompany();
+        var args = GetGameCommandArgs(company, "tax", "10");
 
-        //var commandHandler =
-        //    new TaxCommandHandler(_gameSettings, companies);
+        var commandHandler =
+            new TaxCommandHandler(GetDefaultGameSettings(), repo, args);
 
-        //var gameCommandArgs =
-        //    new GameCommandArgs("123321", "BROADCASTER", "tax", "10", true, false, false, false);
+        commandHandler.Execute();
 
-        //commandHandler.Execute(gameCommandArgs);
-
-        //Assert.Equal("A 10% tax was applied to all companies", commandHandler.ChatMessages.First());
-        //Assert.True(commandHandler.CompanyDataUpdated);
-        //Assert.Equal(100, companies.First(c => c.Value.IsBroadcaster).Value.Points);
-        //Assert.Equal(90, companies.First(c => !c.Value.IsBroadcaster).Value.Points);
+        commandHandler.ChatMessages.First()
+            .ShouldBe("A 10% tax was applied to all companies");
     }
 }
